8a0ffa6640793bd1770edfbb716e746ecf906f79
Avimitin <avimitin@gmail.com>
2022-09-19 19:04:22 +0800

    melon: implement response type
    Signed-off-by: Avimitin <avimitin@gmail.com>

---
+++ src/req/melon.rs
@@ -0,0 +1,72 @@
+#![allow(unused)]
+use std::collections::HashMap;
+
+use serde::{Deserialize, Deserializer};
+
+#[derive(Deserialize)]
+pub struct Response {
+    #[serde(rename = "workList")]
+    worklist: Vec<WorkList>,
+    #[serde(rename = "markList")]
+    marklist: Vec<MarkList>,
+}
+
+#[derive(Deserialize)]
+pub struct WorkList {
+    alias: Box<str>,
+    packages: Vec<Box<str>>,
+}
+
+#[derive(Deserialize)]
+pub struct MarkList {
+    name: Box<str>,
+    marks: Vec<Mark>,
+}
+
+#[derive(Deserialize)]
+pub struct Mark {
+    name: Box<str>,
+    #[serde(deserialize_with = "flatten")]
+    by: Box<str>,
+    comment: Box<str>,
+}
+
+fn flatten<'de, D>(d: D) -> Result<Box<str>, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let s: HashMap<String, String> = HashMap::deserialize(d)?;
+
+    let alias: Box<str> = s["alias"]
+        .as_str()
+        .into();
+
+    Ok(alias)
+}
+
+#[test]
+fn test_flatten() {
+    let raw = r#"   {
+      "name": "bear",
+      "marks": [
+        {
+          "name": "failing",
+          "by": {
+            "alias": "null (bot)"
+          },
+          "comment": "2022/9/4 15:25:49 (UTC+8)"
+        },
+        {
+          "name": "noqemu",
+          "by": {
+            "alias": "Moody"
+          },
+          "comment": ""
+        }
+      ]
+    }"#;
+
+    let marklist: MarkList = serde_json::from_str(raw).unwrap();
+    let marker = &marklist.marks[1].by;
+    assert_eq!(marker.as_ref(), "Moody");
+}
--- src/req/mod.rs
+++ src/req/mod.rs
@@ -1,1 +1,2 @@
 pub mod felix;
+pub mod melon;
